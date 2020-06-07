45fad75e147f10e191ddf76400aa08a747d03629
Nabz <bnnabiswa@gmail.com>
2020-06-07 05:56:20 +0300

    add Profile model

--- appScore/models.py
+++ appScore/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to='images/')
+    bio =  models.TextField(blank=True)
+    contact = models.CharField(max_length=80 ,null=True)
+
+    def __str__(self):
+            return self.bio
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
