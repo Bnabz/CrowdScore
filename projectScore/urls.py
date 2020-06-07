cf1bd4f3c2b5e5c75a48ad1aa22b65b3c0a41a45
Nabz <bnnabiswa@gmail.com>
2020-06-07 17:34:31 +0300

    include registration, login,logout urls

--- projectScore/urls.py
+++ projectScore/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views
+from django.contrib.auth.views import LoginView, LogoutView
+from django_registration.backends.one_step.views import RegistrationView
+from django.urls import reverse_lazy
 
 urlpatterns = [
     path('', include('appScore.urls')),
     path('admin/', admin.site.urls),
+    path('accounts/register/',RegistrationView.as_view(success_url=reverse_lazy('my_profile')),name='django_registration_register'),
+    path('accounts/', include('django_registration.backends.one_step.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', LoginView.as_view(), name='login'),
+    path('accounts/logout/', LogoutView.as_view(), name='logout'),
 ]
