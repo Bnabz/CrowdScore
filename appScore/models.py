8938f7a84a771b13f4cd3f5ffcbcdf1984407df9
Nabz <bnnabiswa@gmail.com>
2020-06-07 06:17:49 +0300

    create project model

--- appScore/models.py
+++ appScore/models.py
@@ -8,11 +8,30 @@ class Profile(models.Model):
     contact = models.CharField(max_length=80 ,null=True)
 
     def __str__(self):
-            return self.bio
+        return f'{self.user.username}
 
     def save_profile(self):
         self.save()
 
     def delete_profile(self):
         self.delete()
+
+class Project(models.Model):
+    title = models.CharField(max_length=50)
+    image = models.ImageField()
+    description = models.CharField(max_length=200)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    date_posted = models.DateTimeField(auto_now_add=True, blank=True)
+    link = models.URLField(max_length=250)
+
+    def __str__(self):
+        return self.title
+    
+
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+
 
