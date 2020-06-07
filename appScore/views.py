089666ea04c44e96696b0a960b86d029f64f31f0
Nabz <bnnabiswa@gmail.com>
2020-06-07 17:25:27 +0300

    include material bootstrap link

--- appScore/views.py
+++ appScore/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse, Http404,HttpResponseRedirect
-
+from .models import Profile, Project
 from django.contrib.auth.decorators import login_required
 
 from django.contrib.auth.models import User
--- projectScore/urls.py
+++ projectScore/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('appScore/', include('appScore.urls')),
+    path('', include('appScore.urls')),
     path('admin/', admin.site.urls),
 ]
