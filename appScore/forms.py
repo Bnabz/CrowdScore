1107d02156c85c29014b0a985312bf51a73e682f
Nabz <bnnabiswa@gmail.com>
2020-06-08 19:05:20 +0300

    add static media settings

--- appScore/forms.py
+++ appScore/forms.py
@@ -3,9 +3,11 @@ from .models import Project,Profile
 
 class ProfileForm(forms.ModelForm):
     class Meta:
+        model = Profile
         fields = ['profile_pic', 'bio']
 
 class ProjectForm(forms.ModelForm):
     class Meta:
+        model = Project
         exclude = ['date_posted']
        
--- appScore/urls.py
+++ appScore/urls.py
@@ -10,3 +10,5 @@ urlpatterns = [
     path('submit_project/', views.submit_project, name='submit_project'),
     path('search/',views.search_results, name='search'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
