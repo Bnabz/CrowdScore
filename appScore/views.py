f42d16e47081b064e8ada04ae84ebf8b8f0c1ffe
Nabz <bnnabiswa@gmail.com>
2020-06-08 06:45:51 +0300

    create my_profile function in views.py

--- appScore/views.py
+++ appScore/views.py
@@ -11,3 +11,16 @@ def index(request):
     projects = Project.objects.all()
     return render(request, 'index.html', {"projects":projects})
 
+
+
+@login_required(login_url='/accounts/login/')
+def my_profile(request):
+    current_user = request.user
+    id=request.user.id 
+    profile = Profile.objects.get(id=id)
+    projects = Project.objects.filter(user__id=id)
+    username = profile.user.username
+    project_number = len(projects)
+    
+
+    return render(request, 'my_profile.html',{"profile":profile,"projects":projects,"current_user":current_user,"username":username,"project_number":project_number})
